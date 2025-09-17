1a11fec9ae78692baee27a8684a3437a408f02dc
Muhammad Taha Ansari <muhammadtahaansari89@gmail.com>
2025-09-17 12:42:27 +0200

    Time: 0 ms (100%), Space: 18 MB (21.94%) - LeetHub

--- 0755-reach-a-number/0755-reach-a-number.py
+++ 0755-reach-a-number/0755-reach-a-number.py
@@ -1,8 +1,19 @@
 class Solution:
     def reachNumber(self, target: int) -> int:
-        t, m, S = abs(target), 0, 0
-        while S < t or (S-t)%2 != 0:
-            m+=1
-            S+=m
-        return m
+        target = abs(target)
+        left = 1
+        right = target
+        while left < right:
+            min_steps = left + (right - left) // 2
+            if min_steps * (min_steps + 1) // 2 >= target:
+                right = min_steps
+            else:
+                left = min_steps + 1
+        min_steps = left
+
+        diff = min_steps * (min_steps + 1) // 2 - target
+        if diff % 2:
+            min_steps += min_steps % 2 + 1
+        return min_steps
+
         
