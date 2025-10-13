a4900b93a50cbd366a6aa4cd5d9733928f1390ab
Muhammad Taha Ansari <muhammadtahaansari89@gmail.com>
2025-10-13 12:25:51 +0200

    Time: 2 ms (77.13%), Space: 17.6 MB (97.51%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
@@ -1,20 +1,15 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        stack1 = [] # to determine valid parantheses
-        stack2 = [] # to remove outer parantheses of valid
-        stack3 = [] # answer
-        for char in s: # the loop must be stop when a valid is found and further actions continue
-            stack2.append(char)
-            if char == "(":
-                stack1.append(")")
-            elif char == ")":
-                stack1.pop()
-            while not stack1 and stack2:
-                stack2.pop()
-                for _ in range(len(stack2)):
-                    stack1.append(stack2.pop())
-                stack1.pop()
-                for _ in range(len(stack1)):
-                    stack3.append(stack1.pop())
-        return "".join(stack3)
+        res=''
+        stack=[]
+        for i in range(len(s)):
+            if s[i]=='(':
+                stack.append(s[i])
+                if len(stack)>1:
+                    res+=s[i]
+            elif s[i]==')':
+                if len(stack)>1:
+                    res+=s[i]
+                stack.pop()
+        return res
         
