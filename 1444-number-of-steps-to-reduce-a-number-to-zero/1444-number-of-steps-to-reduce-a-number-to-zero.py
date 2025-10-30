d10975c4b436d4838a93f35f5f15a7015b760ad8
Muhammad Taha Ansari <muhammadtahaansari89@gmail.com>
2025-10-30 16:17:16 +0100

    Time: 0 ms (100%), Space: 18 MB (18.16%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,9 +1,12 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
-        if num == 0: # base case
-            return 0
-        if num % 2 == 0:
-            return 1 + self.numberOfSteps(num // 2) # recursive call for even n
-        else:
-            return 1 + self.numberOfSteps(num - 1) # recursive call for odd n
+        steps = 0
+        while num != 0:
+            if num % 2 == 0:
+                num = num//2
+                steps += 1
+            else:
+                num = num - 1
+                steps += 1
+        return steps
         
