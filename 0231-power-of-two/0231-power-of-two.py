155a6a9ba703c316a22219e465f13c55ef5eb36d
Muhammad Taha Ansari <muhammadtahaansari89@gmail.com>
2025-11-17 16:55:30 +0100

    Time: 3 ms (14.57%), Space: 17.9 MB (35.42%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,10 +1,8 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n == 1:
-            return True # base case
-        elif n % 2 !=0:
+        if n == 0:
             return False
-        elif n == 0:
-            return False
-        return self.isPowerOfTwo(n//2) # Recursive Call
+        while n % 2 ==0: # checks till the number even
+            n = n // 2
+        return n == 1
         
